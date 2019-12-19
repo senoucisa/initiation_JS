6e9eace3610a2f76df6863a14836445c1d28686c
senoucisa <58477081+senoucisa@users.noreply.github.com>
2019-12-19 09:22:37 +0100

    initiationjavascript

---
+++ js/01_base_js.js
@@ -0,0 +1,27 @@
+/***************** 
+ * synthaxe de base
+ * *****************/
+
+
+
+
+// 1.je verifie toujours que ma pge js et mon fichier html sont connectés:
+alert('ok js');
+
+// deux slashes pour un commentaire sue une ligne (ctrl +/)
+/*
+(shift+alt+a)
+ici un 
+commentaire sur
+plusieurs lignes
+
+/*
+/*
+2 maniéres d'afficher une boite de dialogue:
+
+    =>alert('super,tu es arrivée sur mon site!');
+    =>prompt('question:on est quel jour?');avec la fonction prompt je dialogue directement avec l'utilisateur
+*/
+
+
+
